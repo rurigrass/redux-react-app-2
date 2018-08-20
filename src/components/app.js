38c1e319a81a1c0e928aaa82c3feaa5b1e71a568
rurigrass <ruripg@gmail.com>
2018-08-21 00:21:20 +0100

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Component } from 'react';
+import BookList from '../containers/book-list';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Booklist />
+      </div>
+    );
+  }
+}
+export default App
